7ace215e25d063c98a0dff22a94e5e3e7f4601cf
Raymon Cao <rcao5@jhu.edu>
2017-03-17 09:29:17 -0400

    preliminary heatmap using ComplexHeatmap package

--- finalDensities.r
+++ finalDensities.r
@@ -1,36 +1,46 @@
 library(CancerInSilico)
+library(ComplexHeatmap)
 
-filenames <- c("./sims_output/output_CIS_Adist_30_grAtoB_75.rds")
+# filenames <- c("./sims_output/output_CIS_Adist_30_grAtoB_75.rds")
+rdsFiles <- list.files(path = "./sims_output", full.names = TRUE, recursive = TRUE)
+rdsFiles <- matrix(rdsFiles, nrow = 11, byrow = TRUE)
+final_density_mat <- matrix(nrow = 11, ncol = 9)
 
-
-for (rds_file in filenames) {
-
-	# read in cell model object
-	CellModelObj <- readRDS(rds_file)
-
-	# get final state
-	finalRow <- tail(CellModelObj@cells, n=1)[[1]]
-
-	# radii lengths
-	radii_lengths <- finalRow[seq(from=3,to=length(finalRow),by=7)]
-
-	# sanity check
-	stopifnot(length(radii_lengths) == length(finalRow)/7)
-	numOfCells <- length(radii_lengths)
-
-	# vector to hold cell type counts
-	celltype_counts <- c(0,0)
-
-	# count number of each celltype
-	for (i in c(1:numOfCells)) {
-
-		if (radii_lengths[i] > 0) {
-			celltype_counts[finalRow[i*7] - 64] <- celltype_counts[finalRow[i*7] - 64] + 1
-		}
-
-	}
-
-	finalDensity <- celltype_counts[1]/sum(celltype_counts)
-
-	# Store in data frame
-  }
+for (row in seq(1,11)) {
+  for (col in seq(1,9)) {
+    
+    # get file name (need to rename files for correct order)
+    rds_file <- rdsFiles[row,col]
+  
+  	# read in cell model object
+  	CellModelObj <- readRDS(rds_file)
+  
+  	# get final state
+  	finalRow <- tail(CellModelObj@cells, n=1)[[1]]
+  
+  	# radii lengths
+  	radii_lengths <- finalRow[seq(from=3,to=length(finalRow),by=7)]
+  
+  	# sanity check
+  	stopifnot(length(radii_lengths) == length(finalRow)/7)
+  	numOfCells <- length(radii_lengths)
+  
+  	# vector to hold cell type counts
+  	celltype_counts <- c(0,0)
+  
+  	# count number of each celltype
+  	for (i in c(1:numOfCells)) {
+  
+  		if (radii_lengths[i] > 0) {
+  			celltype_counts[finalRow[i*7] - 64] <- celltype_counts[finalRow[i*7] - 64] + 1
+  		}
+  
+  	}
+  
+  	finalDensity <- celltype_counts[1]/sum(celltype_counts)
+  
+  	# Store in data frame
+  	final_density_mat[row,col] <- finalDensity
+    }
+}
+Heatmap(matrix=final_density_mat)
