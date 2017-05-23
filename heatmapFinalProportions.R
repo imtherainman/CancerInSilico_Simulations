d1e7853993f85ddd536199cc1f51c0b943264ff4
rcao5 <rcao5@jhu.edu>
2017-05-23 16:01:45 -0400

    return list of 3-tuples : (init_proportion_typeA, cycle_length_typeB, finalProportions)

--- heatmapFinalProportions.R
+++ heatmapFinalProportions.R
@@ -4,30 +4,32 @@ library(methods)
 
 getFinalProportionMat <- function(list_of_cell_models, num_rows) {
 
-	mat_of_cell_models <-  matrix(list_of_cell_models, nrow = num_rows, byrow = TRUE)
-	num_cols <- dim(mat_of_cell_models)[2]
-	final_proportion_mat <- matrix(nrow = num_rows, ncols = num_cols)
+	final_proportion_data <- list()
 
-	for (r in seq(1,num_rows)) {
-		for (c in seq(1,num_cols)) {
+	for (CellModelObj in list_of_cell_models) {
 
-			CellModelObj <- mat_of_cell_models[r,c]
+		# Params (heatmap axis values)
+		init_proportion_typeA <- CellModelObj@cellTypeInitFreq[[1]]
+		cycle_length_typeB <- CellModelObj@cellTypes[[2]]@minCycle
 
-			finalTime <- CellModelObj@runTime
-			finalCellType_list <- getCellTypes(CellModelObj, finalTime)
-			celltype_counts <- c(0,0)
-			celltype_counts[1] <- sum(finalCellType_list == 1)
-			celltype_counts[2] <- sum(finalCellType_list == 2)
-			finalDensity <- celltype_counts[1]/sum(celltype_counts)
-			final_proportion_mat[r,c] <- finalDensity
-		}
+		# Get final proportion
+		finalTime <- CellModelObj@runTime
+		finalCellType_list <- getCellTypes(CellModelObj, finalTime)
+		celltype_counts <- c(0,0)
+		celltype_counts[1] <- sum(finalCellType_list == 1)
+		celltype_counts[2] <- sum(finalCellType_list == 2)
+		finalProportions <- celltype_counts[1]/sum(celltype_counts)
+
+		# Add to list
+		final_proportion_data[[length(final_proportion_data)+1]] <- c(init_proportion_typeA, cycle_length_typeB, finalProportions)
+		
 	}
 
-	return(final_proportion_mat)
-	
+	return(final_proportion_data)
+
 }
 
-# rownames(final_proportion_mat) <- sapply(strsplit(rdsFiles[,1],split="_"),function(x){paste(x[4],x[5])})
-# colnames(final_proportion_mat) <- sapply(strsplit(rdsFiles[1,],split="_"),function(x){paste(x[6],x[7])})
-# Heatmap(final_proportion_mat[paste('Adist',seq(from=0,to=100,by=10)),paste('grAtoB ',seq(from=0,to=200,by=25),'.rds', sep="")],cluster_columns = F,cluster_rows = F)
-# Heatmap(matrix=final_proportion_mat)
+# rownames(final_proportion_data) <- sapply(strsplit(rdsFiles[,1],split="_"),function(x){paste(x[4],x[5])})
+# colnames(final_proportion_data) <- sapply(strsplit(rdsFiles[1,],split="_"),function(x){paste(x[6],x[7])})
+# Heatmap(final_proportion_data[paste('Adist',seq(from=0,to=100,by=10)),paste('grAtoB ',seq(from=0,to=200,by=25),'.rds', sep="")],cluster_columns = F,cluster_rows = F)
+# Heatmap(matrix=final_proportion_data)
